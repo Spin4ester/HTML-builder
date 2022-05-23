b4ff0bba0fa3d0387daa8dc7a3d31ff7bdf7daff
Spin4ester <bakibayev.r@gmail.com>
2022-05-23 23:24:16 +0200

    solve 06-build-page

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,33 +1,30 @@
 const fs = require('fs');
-// const fse = require('fs-extra');
 const path = require('path');
-const pathFile = path.join(__dirname, './files');
-const pathFileCopy = path.join(__dirname, './files-copy');
-
-function copyDir() {
+const origDir = path.join(__dirname, './files');
+const copyDir = path.join(__dirname, './files-copy');
 
 
-  fs.exists(pathFileCopy, exists => {
-    if (!exists) {
-      fs.mkdir(pathFileCopy, err => {
-        if (err) throw err;
-      });
-    }
-  }  
-  );
-  
-  fs.readdir(pathFile, (err, files) => {
+
+fs.readdir(__dirname, (err) => {
+  if (err) throw err;
+  fs.rm(copyDir, { recursive: true, force: true }, (err) => {
     if (err) throw err;
-    for (let i = 0; i < files.length; i++) {
-      fs.copyFile(path.join(__dirname, 'files', files[i]), path.join(__dirname, 'files-copy', files[i]),
-        err => {
-          if (err) throw err;
-        }
-      ); 
+    fs.mkdir(copyDir, { recursive: true }, (err) => {
+      if (err) throw err;
+    });
+
+    fs.readdir(origDir, (err, files) => {
+      if (err) throw err;
+      for (let i = 0; i < files.length; i++) {
+        fs.copyFile(path.join(__dirname, 'files', files[i]), path.join(__dirname, 'files-copy', files[i]),
+          err => {
+            if (err) throw err;
+          }
+        ); 
+      }
     }
-  }
-  );   
-}
-  
-copyDir();
+    );  
+  });
+});
+
 
---
+++ 06-build-page/index.js
@@ -0,0 +1,111 @@
+const fs = require('fs');
+const path = require('path');
+
+
+//! COPY DIRECTORY
+
+
+function cloneDir(origDir, copyDir) {
+
+    fs.rm(copyDir, { recursive: true, force: true }, (err) => {
+      if (err) throw err;
+    
+      fs.mkdir(copyDir, { recursive: true }, () => {});
+  
+      fs.readdir(origDir, { withFileTypes: true }, (_, files) => {
+        files.forEach((file) => {
+  
+          const fileName = file.name;
+          const fileOrigPath = path.join(origDir, fileName);
+          const fileCopyPath = path.join(copyDir, fileName);
+    
+          //checking if file or folder
+          if (file.isFile()) {
+            fs.promises.copyFile(fileOrigPath, fileCopyPath);
+          } else {
+            cloneDir(fileOrigPath, fileCopyPath);
+          }
+        });
+      });
+    });
+  }
+
+
+//! HMTL Bundle
+
+async function bundleHTML() {
+  const template = await fs.promises.readFile(
+    path.join(__dirname, './template.html')
+  );
+  
+  let html = template.toString();
+  
+  const components = await fs.promises.readdir(
+    path.join(__dirname, './components')
+  );
+  
+  for (const comp of components) {
+    const compPath = path.join(__dirname, './components', comp);
+    const compInner = await fs.promises.readFile(compPath);
+    const compName = comp.split('.')[0];
+    const compValue = compInner.toString();
+    html = html.replace(`{{${compName}}}`, compValue);
+  }
+
+  await fs.promises.writeFile(
+    path.join(__dirname, './project-dist', 'index.html'), html);
+}
+
+
+//! CREATING CSS FILE
+
+function bundleStyle() {
+
+  fs.readdir(path.join(__dirname, './styles'), (err, files) => {
+    files.forEach(file => {
+      let x = file.slice(file.length-3,file.length);
+      if(x !== 'css') {
+        return;
+      } else {
+        
+        fs.readFile(path.join(__dirname, './styles', file), (err, style) => {
+          if (err) throw err;
+         
+          createBundle(style);
+        });   
+      }
+    });
+  });
+    
+  function createBundle(file) {
+    fs.open(path.join(__dirname, './project-dist', 'style.css'),
+      'w',
+      err => {
+        if (err) throw err;
+      }
+    );
+    
+    fs.appendFile(path.join(__dirname, './project-dist', 'style.css'), file, (err) => {
+      if (err) throw err;
+      console.log('Styles created');
+    });
+  }
+  
+}
+  
+
+
+
+//! Final build
+
+function htmlBuild() {
+  fs.promises.mkdir(path.join(__dirname, './project-dist'), {recursive: true});
+  cloneDir(
+    path.join(__dirname, './assets'),
+    path.join(__dirname, './project-dist', '/assets')
+  );
+  bundleHTML();
+  bundleStyle();
+}
+
+htmlBuild();
